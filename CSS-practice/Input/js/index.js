b3a5eea15c898df6d85db5c3bb6f94bbe46e8d6b
GiveMe-A-Name <a455300764b@hotmail.com>
2020-10-13 15:58:31 +0800

    更新使用onchange事件

--- CSS-practice/Input/js/index.js
+++ CSS-practice/Input/js/index.js
@@ -1,31 +1,14 @@
 let form = document.forms.inputForm;
-let label1= document.querySelector('label.label1'),
+let label1 = document.querySelector('label.label1'),
 label2 = document.querySelector('label.label2');
 
-form.input1.onfocus = function(){
-	if(!label1.classList.contains('hava-content')){
+form.elements['input1'].onchange = function(event){
+	if(event.target.value != ""){
 		label1.classList.add('hava-content');
-		label1.style.fontSize="10px";
 	}
 }
-
-form.input1.onblur = function(){
-	if(!form.input1.value){
-		label1.classList.remove('hava-content');
-		label1.style.fontSize="14px";
-	}
-}
-
-form.input2.onfocus = function(){
-	if(!label2.classList.contains('hava-content')){
+form.elements.input2.onchange = function(event){
+	if(evet.target.value != ""){
 		label2.classList.add('hava-content');
-		label2.style.fontSize="10px";
-	}
-}
-
-form.input2.onblur = function(){
-	if(!form.input2.value){
-		label2.classList.remove('hava-content');
-		label2.style.fontSize="14px";
 	}
 }
